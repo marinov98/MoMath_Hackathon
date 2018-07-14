8bd7fa4a8bece831aaad5a9ae6ce265b9ed508c5
Vanessa Sun <Vanessa@HON-2016463.local>
2018-07-14 12:46:49 -0400

    adding chessboard

---
+++ knightsTour/chessboard.js
@@ -0,0 +1,72 @@
+function setup() {
+  createCanvas(576, 576);
+}
+
+function draw() {
+  // background
+	background(255, 255, 255);
+
+	// full board
+	fill(232, 235, 225);
+	stroke (0, 0, 0);
+	rect(80, 90, 424, 424);
+
+	// square 1
+	fill(255, 255, 255);
+	rect(80, 90, 53, 53);
+	// square 2
+	// square 3
+	// square 4
+	// square 5
+	// square 6
+	// square 7
+	// square 8
+	// square 9
+	// square 10
+	// square 11
+	// square 12
+	// square 13
+	// square 14
+	// square 15
+	// square 16
+	// square 17
+	// square 18
+	// square 19
+	// square 20
+	// square 21
+	// square 22
+	// square 23
+	// square 24
+	// square 25
+	// square 26
+	// square 27
+	// square 28
+	// square 29
+	// square 30
+	// square 31
+	// square 32
+	// square 33
+	// square 34
+	// square 34
+	// square 36
+	// square 37
+	// square 38
+	// square 39
+	// square 40
+	// square 41
+	// square 42
+	// square 43
+	// square 44
+	// square 45
+	// square 46
+	// square 47
+	// square 48
+	// square 49
+	// square 50
+	// square 2
+	// square 3
+	// square 4
+	// square 2
+	// square 3
+	// square 4
+};
