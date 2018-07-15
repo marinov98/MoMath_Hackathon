343d483fbef54d1052a05bcb10e11ae56d9ab89e
Vanessa Sun <Vanessa@HON-2016463.local>
2018-07-15 01:37:37 -0400

    GOLDEN TRASH

--- math-square/behs/testknight.js
+++ math-square/behs/testknight.js
@@ -15,10 +15,12 @@ const pb = new P5Behavior();
 
 // for WEBGL: pb.renderer = 'webgl';
 
+var goldenknight
 
 pb.preload = function (p) {
   /* this == pb.p5 == p */
   // ...
+goldenknight= this.loadImage('images/goldentrash2.png')
 }
 
 pb.setup = function (p) {
@@ -34,29 +36,33 @@ pb.draw = function (floor, p) {
   // draw here...
   this.clear();
 
+this.fill(0)
   this.rect(0, 0, this.width, this.height);
 
-this.fill(0, 0, 0)
-this.rect(0, 0, 576, 576)
 
-// shadow
-this.fill(255, 255, 255);
-this.rect(113, 95, 424, 424);
+
+  // shadow
+  this.noStroke()
+  this.fill(182, 255, 224);
+  this.rect(117, 98, 424, 424);
 
 // Title
+this.stroke(232, 170, 21);
+this.fill(232, 170, 21);
+this.strokeWeight(1);
 this.textSize(36);
-this.textFont('Palatino');
-this.text("Knight's Tour", 215, 60);
+this.textFont('Courier New');
+this.text("The Golden Knight", 140, 60);
 
 // Underthis.line
-this.stroke(255, 255, 255);
+this.stroke(232, 253, 88);
 this.strokeWeight(2);
-this.line(215, 65, 430, 65);
+this.line(130, 65, 520, 65);
 
 // full board
-this.fill(232, 235, 225);
 this.stroke (0, 0, 0);
-this.rect(110, 90, 424, 424);
+this.fill(232, 235, 225);
+
 
 // square 1
 this.fill(255, 255, 255);
@@ -287,15 +293,15 @@ this.fill(178, 235, 206);
 this.rect(110, 461, 53, 53);
 
 // square 58
-this.fill(255, 255, 255);
+this.fill(61, 104, 100);
 this.rect(163, 461, 53, 53);
 
 // square 59
-this.fill(232, 235, 225);
+this.fill(98, 168, 161);
 this.rect(216, 461, 53, 53);
 
 // square 60
-this.fill(255, 255, 255);
+this.fill(142, 25, 22);
 this.rect(269, 461, 53, 53);
 
 // square 61
@@ -314,57 +320,85 @@ this.rect(428, 461, 53, 53);
 this.fill(255, 255, 255);
 this.rect(481, 461, 53, 53);
 
+// Golden Knight
+this.image(goldenknight, 105, 457, 65, 60)
+
 // restart button
 this.textSize(14);
 this.textFont('Helvetica');
 this.fill(255, 255, 255);
-this.text("RESTART", 21, 141);
+this.text("RESTART", 21, 191);
 
 //arrow
-this.stroke(0,0, 0)
+this.stroke(0, 0, 0)
 this.strokeWeight(2);
 this.fill(182, 255, 224);
-this.ellipse(50, 100, 50, 50);
+this.ellipse(50, 150, 50, 50);
 
-this.line(51, 105, 49, 110);
-this.line(49, 110, 54, 114);
+this.line(51, 155, 49, 160);
+this.line(49, 160, 54, 164);
 this.stroke(0, 0, 0);
 this.strokeWeight(3);
 this.noFill();
-this.arc(50, 100, 20, 20, Math.PI, Math.PI/2);
+this.arc(50, 150, 20, 20, Math.PI, Math.PI/2);
 
 
 // undo button
 this.textSize(14);
 this.textFont('Helvetica');
 this.fill(255, 255, 255);
-this.text("UNDO", 28, 241);
+this.text("UNDO", 28, 305);
 this.fill(141, 235, 206);
-this.ellipse(50, 200, 50, 50);
+this.ellipse(50, 265, 50, 50);
+
+// arrow
+this.stroke(0, 0, 0);
+this.strokeWeight(3);
+
+// arrow
+this.stroke(0, 0, 0);
+this.strokeWeight(3);
+this.line(42, 261, 42, 271)
+this.line(42, 271, 61, 271)
+this.line(63, 271, 42, 271)
+
+this.line(63, 271, 60, 266)
+this.line(63, 271, 60, 275)
+
 
 // redo button
 this.textSize(14);
 this.textFont('Helvetica');
 this.fill(255, 255, 255);
-this.text("REDO", 28, 341);
+this.text("REDO", 28, 416);
 this.fill(111, 184, 162);
-this.ellipse(50, 300, 50, 50);
+this.ellipse(50, 375, 50, 50);
 
-// help button
-this.textSize(14);
-this.textFont('Helvetica');
-this.fill(255, 255, 255);
-this.text("HELP", 30, 441);
-this.fill(91, 155, 132);
-this.ellipse(50, 400, 50, 50);
+// arrow
+this.stroke(0, 0, 0);
+this.strokeWeight(3);
+this.line(42, 371, 42, 381)
+this.line(42, 381, 61, 381)
+this.line(63, 381, 42, 381)
+
+this.line(63, 381, 60, 376)
+this.line(63, 381, 60, 385)
 
 //give up button
 this.textSize(14);
 this.textFont('Helvetica');
 this.fill(255, 255, 255);
-this.text("GIVE UP", 21, 541);
+this.text("GIVE UP", 22, 516);
+this.text("(on life)", 26, 529)
 this.fill(48, 79, 70);
-this.ellipse(50, 500, 50, 50);
+this.ellipse(50, 475, 50, 50);
+
+this.stroke(0, 0, 0);
+this.strokeWeight(3);
+this.line(50, 460, 50, 485);
+
+this.line(42, 480, 50, 485);
+this.line(58, 480, 50, 485);
 
 // Start text
 this.textSize(30);
@@ -373,13 +407,33 @@ this.fill(255, 255, 255);
 this.text("START", 280, 555);
 
 // start arrow
-this.stroke(255, 255, 255)
-this.strokeWeight(2)
-this.noFill()
-this.arc(200, 555, 20, 20, Math.PI, Math.PI/2);
+
+// point
+this.stroke(255, 255, 255);
+this.strokeWeight(6)
+this.line(140, 530, 130, 535)
+this.line(140, 530, 150, 535)
+
+// extension
+this.stroke(255, 255, 255);
+this.strokeWeight(6)
+this.line(140, 530, 140, 540);
+
+// right straight
+this.stroke(255, 255, 255);
+this.strokeWeight(6);
+this.line(150, 550, 245, 550);
+
+// curve
+this.stroke(255, 255, 255);
+this.strokeWeight(6);
+this.noFill();
+this.arc(150, 540, 20, 20, Math.PI/2, Math.PI);
 
 
 };
+
+
 
 export const behavior = {
   title: "Sensor Debug (P5)",
